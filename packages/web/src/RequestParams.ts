c6fde54eb2f977ba964eee3ba0030fbe089d539b
Mackie Underdown <macklinu@users.noreply.github.com>
2026-01-03 15:00:21 -0500

    Add number to RequestParams value type (#12)

--- packages/web/src/RequestParams.ts
+++ packages/web/src/RequestParams.ts
@@ -3,4 +3,4 @@ import * as Context from 'effect/Context'
 /** An Effect service for obtaining route URL params. */
 export class RequestParams extends Context.Tag(
   '@macklinu/effect-web/RequestParams'
-)<RequestParams, Record<string, string | undefined>>() {}
+)<RequestParams, Record<string, string | number | undefined>>() {}
